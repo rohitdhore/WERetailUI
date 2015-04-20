9e1a5a44968c9065ff3b221dd9ca5eec22a69582
Rohit Dhore <rohit.dhore1612@gmail.com>
2015-04-21 00:10:11 +0530

    login screen started

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,8 +20,7 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        templateUrl: 'views/Login.html'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
